f8f3a9cee6483e93883925d93e9ec322cbbf0dee
strhzy <alexeydm2@gmail.com>
2024-04-15 14:29:38 +0300

    настроил страницы и карточку

--- calendar/obj/Debug/net8.0-windows/calendar.AssemblyInfo.cs
+++ calendar/obj/Debug/net8.0-windows/calendar.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("calendar")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2bac514b044ef7e55a4ef356cc480fb83711a8c0")]
 [assembly: System.Reflection.AssemblyProductAttribute("calendar")]
 [assembly: System.Reflection.AssemblyTitleAttribute("calendar")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
