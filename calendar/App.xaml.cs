2bac514b044ef7e55a4ef356cc480fb83711a8c0
strhzy <alexeydm2@gmail.com>
2024-04-15 13:58:41 +0300

    первый коммит

---
+++ calendar/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace calendar
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
